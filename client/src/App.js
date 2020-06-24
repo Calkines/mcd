e88d4e9c9938acefcab0eec141b77f9b42282dba
Rafael CP <rafael_it@hotmail.com>
2020-06-23 22:56:35 -0300

    fix: force build

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ function App() {
     <Router>
       <div className="App">
       <header className="App-header">
-        <h1> Welcome to page</h1>
+        <h1> Welcome simple app</h1>
         <Link to="/">Home</Link>
         <Link to="/otherpage">Other Page</Link>
       </header>
